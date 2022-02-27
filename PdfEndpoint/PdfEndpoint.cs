cd881c07600f63ea757e1b57fe4facba8528743f
Aleksander Jess <jess.aleksander@gmail.com>
2022-02-27 17:41:48 +0100

    await using

--- PdfEndpoint/PdfEndpoint.cs
+++ PdfEndpoint/PdfEndpoint.cs
@@ -13,35 +13,36 @@ public class PdfEndpoint : Endpoint<MyRequest> {
     
     public override async Task HandleAsync(MyRequest req, CancellationToken ct) {
 
-         MemoryStream stream = new MemoryStream();
-            PdfWriter writer = new PdfWriter(stream);
-            PdfDocument pdf = new PdfDocument(writer);
-            Document doc = new Document(pdf);
+         await using (MemoryStream stream = new MemoryStream()) {
+             PdfWriter writer = new PdfWriter(stream);
+             PdfDocument pdf = new PdfDocument(writer);
+             Document doc = new Document(pdf);
 
-            var font = PdfFontFactory.CreateFont(StandardFonts.TIMES_ROMAN);
-            doc.SetFont(font);
-            doc.Add(new Paragraph("123"));
-            
-            List list = new List()
-                .SetSymbolIndent(12)
-                .SetListSymbol("u2022")
-                .SetFont(font);
-            // Add ListItem objects
-            list.Add(new ListItem("Never gonna give you up"))
-                .Add(new ListItem("Never gonna let you down"))
-                .Add(new ListItem("Never gonna run around and desert you"))
-                .Add(new ListItem("Never gonna make you cry"))
-                .Add(new ListItem("Never gonna say goodbye"))
-                .Add(new ListItem("Never gonna tell a lie and hurt you"));
-            // Add the list
-            doc.Add(list);
-            
-            // create a sample pdf with iText
+             var font = PdfFontFactory.CreateFont(StandardFonts.TIMES_ROMAN);
+             doc.SetFont(font);
+             doc.Add(new Paragraph("123"));
+
+             List list = new List()
+                 .SetSymbolIndent(12)
+                 .SetListSymbol("u2022")
+                 .SetFont(font);
+             // Add ListItem objects
+             list.Add(new ListItem("Never gonna give you up"))
+                 .Add(new ListItem("Never gonna let you down"))
+                 .Add(new ListItem("Never gonna run around and desert you"))
+                 .Add(new ListItem("Never gonna make you cry"))
+                 .Add(new ListItem("Never gonna say goodbye"))
+                 .Add(new ListItem("Never gonna tell a lie and hurt you"));
+             // Add the list
+             doc.Add(list);
+
+             // create a sample pdf with iText
 
 
-            
-            await SendBytesAsync(stream.GetBuffer(), contentType:"application/pdf", cancellation: new CancellationToken());
-            
+
+             await SendBytesAsync(stream.GetBuffer(), contentType: "application/pdf",
+                 cancellation: new CancellationToken());
+         }
         
     }
 
